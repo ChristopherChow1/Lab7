53dc58b68d7870e26eace74bb32db4d9278d915a
Christopher Chow <Christopher.Chow-1@ou.edu>
2019-03-12 14:15:51 -0500

    commit 5: revised the compareToImp1 in both SickPerson.java and HealthyPerson.java

--- HealthyPerson.java
+++ HealthyPerson.java
@@ -3,13 +3,25 @@ public class HealthyPerson extends Person{
 	String reason;
 	public HealthyPerson(String name, int age, String reason) {
 		super(name, age);
+		//severity = 0;
 		this.reason = reason;
 		// TODO Auto-generated constructor stub
 	}
 	@Override
 	protected int compareToImpl(Person p) {
 		// TODO Auto-generated method stub
-		return 1;
+		HealthyPerson name = (HealthyPerson) p;
+		if (!(p instanceof HealthyPerson)) {
+			return 0;
+		} else {
+			if (this.getAge() < name.getAge()) {
+				return -1;
+			} else if (this.getAge()>name.getAge()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 	}
 	public String toString() {
 		return String.format("%s, a %d-year old who is in the hospital because %s.", getName(), getAge(), reason);
--- SickPerson.java
+++ SickPerson.java
@@ -10,9 +10,26 @@ public class SickPerson  extends Person{
 	@Override
 	protected int compareToImpl(Person p) {
 		// TODO Auto-generated method stub
-		
-		return -1;
+		SickPerson s = (SickPerson) p;
+		if (!(p instanceof SickPerson)) {
+			return 0;
+		} else {
+			if (this.severity > s.GetSeverity()){
+				return 1;
+			} else if (this.severity< s.GetSeverity()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+		//return severity.compareTo(p.severity);
+		 
 	}
+	private int GetSeverity() {
+		// TODO Auto-generated method stub
+		return severity;
+	}
+
 	public String toString() {
 		return String.format("%s, a %d-year old whose illness has a severity of %d .", getName(), getAge(), severity);
 	}
