ce404aa5fa24178a6db2099d86438444856bf9a4
Christopher Chow <Christopher.Chow-1@ou.edu>
2019-03-12 16:02:25 -0500

    commit 6: created SickPersonTest

---
+++ SickPersonTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+//import org.junit.Assert.*;
+
+import org.junit.Test;
+
+//import junit.framework.Assert;
+
+public class SickPersonTest {
+
+	private int severity;
+
+	@Test
+	public void test() {
+		// VV the code below tests the parameters of SickPerson VV
+		Person SPT = new SickPerson("Tim", 19, severity = 5);
+		Assert.assertEquals("SickPerson name is Wrong","Tim", SPT.getName());
+		Assert.assertEquals("SickPerson age is wrong", 19, SPT.getAge());
+		Assert.assertEquals("SickPerson severity is wrong", 5, this.severity);
+		// VV the code below tests compareTo() VV
+		Person SPT1 = new SickPerson("Tim", 19, severity = 7);
+		Person SPT2 = new SickPerson("Tom", 19, severity = 5);
+		SPT1.compareTo(SPT2);
+		Assert.assertEquals("wrong order", 1, SPT1.compareTo(SPT2));
+		
+	}
+
+
+}
